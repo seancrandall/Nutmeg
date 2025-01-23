9a9fa30e32b052eff32e9d3e31572f5e6458834b
seancrandall <sean@patcapgroup.com>
2025-01-23 14:11:17 -0700

    Create main.cpp

---
+++ nmgui/main.cpp
@@ -0,0 +1,46 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+#include <QPixmap>
+#include <QSplashScreen>
+
+#include "logger.h"
+#include "settings.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QPixmap nutmegSplashScreen(":images/Logo.png");
+    QSplashScreen splash(nutmegSplashScreen);
+    splash.show();
+
+    //Initialize the log file and setup the maximum and "preferred" number of lines
+    //Once log grows beyond max number of lines, it is trimmed back to "preferred" size.
+    Nutmeg::Logger::InitializeLogCount();
+    Nutmeg::Settings settings;
+    Nutmeg::Logger::maxLogLines = settings.maxLogLines;
+    Nutmeg::Logger::preferredLogLines = settings.preferredLogLines;
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages)
+    {
+        const QString baseName = "nmgui_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName))
+        {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    Nutmeg::MainWindow w;
+
+    QIcon icon(":images/nutmeg.ico");
+    w.setWindowIcon(icon);
+
+    w.show();
+    splash.finish(&w);
+    return a.exec();
+}
